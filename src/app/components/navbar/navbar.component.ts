66524adea0f348fbd3a8cf38ab71ea119c9480ea
IvoVM <ivomastrangelo12@gmail.com>
2023-11-30 22:32:05 -0300

    minor changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,20 +9,16 @@ import { Router } from '@angular/router';
 export class NavbarComponent {
   constructor(private router: Router) {}
   isLoggedIn(): boolean {
-    // Verificar si el usuario está autenticado (por ejemplo, si existe en el local storage)
     return !!localStorage.getItem('user');
   }
 
   getUsername(): string | null {
-    // Obtener el nombre de usuario desde el local storage
     const user = JSON.parse(localStorage.getItem('user') || '{}');
     return user.username || null;
   }
 
   logout(): void {
-    // Limpiar la información de autenticación al hacer logout
     localStorage.removeItem('user');
-    // Redirigir al usuario a la página de login u otra página
     this.router.navigate(['/login']);
   }
 }
